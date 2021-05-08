3299bb2ed12f5d543cf28f4acf43b9b4224a1b90
Iman Bains <imanjitbains@yahoo.com>
2021-05-08 16:13:35 -0700

    Added questions and answers

---
+++ js/questions.js
@@ -0,0 +1,34 @@
+var allQuestions = [
+    {
+      title: "Which of these is NOT used for variable declaration?:",
+      choices: ["var", "for", "const", "let"],
+      answer: "for"
+    },
+    {
+      title: "____ is used to stop event bubbling:",
+      choices: ["stopPropagation", "stopBubbling", "stopImmediate", "stopEvent"],
+      answer: "stopPropagation"
+    },
+    {
+      title: "DOM stands for Document Object ____:",
+      choices: [
+        "Method",
+        "Moment",
+        "Material",
+        "Model"
+      ],
+      answer: "Model"
+    },
+    {
+      title:
+        "Which of these is NOT used in a conditional statement?:",
+      choices: ["if", "else", "if else", "else if"],
+      answer: "if else"
+    },
+    {
+      title:
+        "____ is used to cancel the default action of an event",
+      choices: ["defaultPrevented()", "preventDefault()", "defaultCancelled()", "cancelDefault()"],
+      answer: "preventDefault()"
+    }
+  ];
